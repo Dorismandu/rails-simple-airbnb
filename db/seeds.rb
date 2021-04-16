334963ebfb8e72465e98217e8e684c4c4b8d7fd8
Dorothee Wallois <dorothee.wallois@live.fr>
2021-04-16 04:56:38 +0200

    exo bnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Clean up Flat DB"
+Flat.destroy_all
+puts "Flat DB cleaned!"
+
+def flat_name
+  adjs = ["Beautiful", "Hidden", "Secret", "Misty", "Silent", "Large", "Cozy", "Amazing"]
+  nouns = ["flat", "appartment", "villa", "studio", "room", "loft"]
+  precise = ["with a view", "close to the city center", "perfect for family", "by the sea"]
+
+  [adjs.sample, nouns.sample, precise.sample].join(' ')
+end
+
+4.times do 
+  @flat = Flat.new(
+    name: flat_name,
+    address: Faker::Address.street_name ,
+    description: "Great flat with " + Faker::House.room + " that has many " + Faker::House.furniture,
+    price_per_night: rand(30..300),
+    pic_url: "https://source.unsplash.com/random/800x600",
+    number_of_guests: rand(1..6)
+  )
+  @flat.save
+  puts "flat added!"
+end
+puts "seeding done!"
